c3aaea13618895c4758828ff4afe09b7d5480e70
Vinayak Nawdhar <vinayaknawdhar003@gmail.com>
2024-06-01 20:12:01 +0530

    Update Testimonial.jsx

--- app/components/Testimonial.jsx
+++ app/components/Testimonial.jsx
@@ -6,18 +6,18 @@ const Testimonial = () => {
             <h1 className='text-3xl xl:text-5xl font-extrabold text-center font-["Outfit"] tracking-tighter drop-shadow-2xl'>WHAT OUR CUSTOMER SAYS</h1>
             <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6 flex gap-8 flex-wrap">
                 {testimonials.map(test => (
-                    <figure key={test.id} class="bg-[#FEFAF9] p-4 rounded-2xl shadow-2xl h-96 w-96 relative">
+                    <figure key={test.id} class="bg-[#FEFAF9] p-4 rounded-2xl shadow-2xl xl:h-96 xl:w-96 relative pb-20 xl:pb-0">
                         <svg className="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor" />
                         </svg>
                         <blockquote>
-                            <p className="text-xl font-medium text-gray-900 opacity-90">"{test.text}"</p>
+                            <p className="text-sm xl:text-xl font-medium text-gray-900 opacity-90">"{test.text}"</p>
                         </blockquote>
-                        <figcaption className="flex items-center justify-center mt-6 space-x-3 absolute left-1/2 translate-x-[-50%] bottom-10">
-                            <div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                        <figcaption className="flex items-center justify-center mt-6 space-x-3 absolute left-1/2 translate-x-[-50%] xl:bottom-10">
+                            <div className="flex items-center divide-x-2 divide-gray-500">
                                 <img className="w-10 h-10 object-cover object-center mr-3 rounded-full" src={test.image} alt="profile picture" />
 
-                                <div className="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">{test.author}</div>
+                                <div className="pl-3 text-sm font-light text-gray-500">{test.author}</div>
                             </div>
                         </figcaption>
                     </figure>
