0a0ceedef03b095859483f355938bb53f38ff92a
Vinayak Nawdhar <vinayaknawdhar003@gmail.com>
2024-06-02 16:26:01 +0530

    Update Hero.jsx

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -41,7 +41,7 @@ const Hero = () => {
         <div className="hiding"><h1 className="text-[9vw] xl:text-8xl font-extrabold xl:w-[63%] tagline">Wonderland!</h1></div>
         <div className="flex gap-8 py-6 md:py-10">
           <button className="px-4 w-60 py-2 md:px-8 md:py-6 text-sm md:text-xl xl:w-48 font-semibold rounded-full text-white bg-pink-800 hover:bg-pink-700 transition-all duration-500">ORDER NOW</button>
-          <button className="px-4 w-60 py-2 md:px-8 md:py-6 text-sm md:text-xl xl:w-48 font-semibold border-4 border-pink-900 rounded-full">Catering</button>
+          {/* <button className="px-4 w-60 py-2 md:px-8 md:py-6 text-sm md:text-xl xl:w-48 font-semibold border-4 border-pink-900 rounded-full">Catering</button> */}
         </div>
       </div>
       <div className="img-div grid gap-10 pl-16 md:pl-0 justify-start md:justify-center xl:absolute top-0 xl:right-[300px]">
