a7ffba4358706700ac50a4621c4c04bee13dc440
Vinayak Nawdhar <vinayaknawdhar003@gmail.com>
2024-06-02 15:36:32 +0530

    mobile changes fixed

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
         <section id='about' className='py-10 bg-white/70 flex flex-col gap-5'>
             <div className='flex items-center flex-wrap'>
                 <div className='xl:w-1/2 flex flex-col px-8 xl:pl-32 gap-5 mb-10 md:mb-0'>
-                    <h1 className='xl:w-[75%] text-left text-5xl font-extrabold tracking-tighter font-[Archivo]'>NOT JUST ANOTHER COOKIE.</h1>
+                    <h1 className='xl:w-[75%] text-left text-3xl md:text-5xl font-extrabold tracking-tighter font-[Archivo] uppercase'>From cookies to marshmallow , pure bliss.</h1>
                     <p className='xl:w-[85%] font-["Archivo"] tracking-tight opacity-75'>At Dori Dori, we bake the tastiest cookies and craft gourmet marshmallows. Our cookies are baked to perfection, with a crunchy exterior and a soft, chewy center. Each marshmallow is handcrafted using only the finest ingredients, resulting in fluffy clouds of sweetness that melt in your mouth.</p>
                 </div>
                 <div className='overflow-hidden rounded-2xl px-5 md:px-0 xl:h-[500px] xl:w-[500px]'>
@@ -14,7 +14,7 @@ const About = () => {
             </div>
             <div className='flex px-8 xl:flex-row-reverse flex-wrap items-center mt-10'>
                 <div className='xl:w-1/2 flex flex-col gap-5 mb-10 md:mb-0'>
-                    <h1 className='xl:w-[60%] text-left text-5xl font-extrabold tracking-tighter font-[Archivo]'>NOT JUST ANOTHER BAKER.</h1>
+                    <h1 className='xl:w-[60%] text-left text-3xl md:text-5xl font-extrabold tracking-tighter font-[Archivo]'>NOT JUST ANOTHER BAKER.</h1>
                     <div>
                     <p className='xl:w-[75%] font-["Archivo"] tracking-tight opacity-75'>Gourmet marshmallows and cookies are crafted with passion and precision. we believe in elevating the simple pleasure of indulgence into an art form. Our journey began with a vision to redefine the boundaries of flavor and texture, creating treats that tantalize the taste buds and ignite the imagination.</p>
                     <p className='xl:w-[75%] font-["Archivo"] tracking-tight opacity-75'>we're a community united by our love for exceptional treats and shared experiences. Whether you're savoring a moment of indulgence alone or sharing our delights with loved ones, we invite you to join us on a journey of flavor and discovery.</p>
--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -4,76 +4,76 @@ import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 const Products = () => {
     return (
-        <section id='product' className='min-h-screen mt-96 xl:mt-0 xl:pt-32 pb-8'>
+        <section id='product' className='min-h-screen mt-[190px] xl:mt-0 xl:pt-32 pb-8 scroll-m-[120px] md:scroll-m-0'>
             <h1 className='text-6xl text-center font-extrabold tracking-tighter text-black/85 mb-5'>Indulge Your Craving!</h1>
             <h3 className='text-center font-semibold text-xl tracking-tighter'>because sometimes you deserve a treat.</h3>
             <div className='px-2 py-3 xl:px-10 xl:py-4 bg-white/50 mx-5 rounded-xl mt-8'>
-                <h1 className='text-4xl xl:text-5xl mb-8 font-semibold font-["Outfit"]'>Gourmet Marshmallows</h1>
+                <h1 className='text-4xl xl:text-5xl mb-10 font-medium font-["Outfit"] opacity-75'>Gourmet Marshmallows</h1>
                 <div className='flex flex-wrap gap-8 justify-center'>
-                    <div className='w-[350px] h-[350px] overflow-hidden relative product rounded-xl'>
-                        <div className='w-[350px] h-[350px] overflow-hidden product-img '>
+                    <div className='w-[130px] h-[150px] xl:w-[350px] xl:h-[350px] overflow-hidden relative product rounded-xl'>
+                        <div className='w-full h-full overflow-hidden product-img '>
                             <img className='h-full w-full object-cover object-center transition-all duration-500' src="/images/blueberry-gourmet-marshmallow.jpg" alt="" />
                         </div>
-                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-3xl z-20 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all tracking-wide'>Blueberry</p>
+                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-sm xl:text-3xl z-20 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all '>Blueberry</p>
                     </div>
-                    <div className='w-[350px] h-[350px] overflow-hidden relative product rounded-xl'>
-                        <div className='w-[350px] h-[350px] overflow-hidden product-img '>
+                    <div className='w-[130px] h-[150px] xl:w-[350px] xl:h-[350px] overflow-hidden relative product rounded-xl'>
+                        <div className='w-full h-full overflow-hidden product-img '>
                             <img className='h-full w-full object-cover object-center transition-all duration-500' src="/images/caramel-marshmallow.jpg" alt="" />
                         </div>
-                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-3xl z-20 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all tracking-wide'>Caramel</p>
+                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-sm xl:text-3xl z-20 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all tracking-tighter'>Caramel</p>
                     </div>
-                    <div className='w-[350px] h-[350px] overflow-hidden relative product rounded-xl'>
-                        <div className='w-[350px] h-[350px] overflow-hidden product-img '>
+                    <div className='w-[130px] h-[150px] xl:w-[350px] xl:h-[350px] overflow-hidden relative product rounded-xl'>
+                        <div className='w-full h-full overflow-hidden product-img '>
                             <img className='h-full w-full object-cover object-center transition-all duration-500' src="/images/cookie-and-cream-marshmallow.jpg" alt="" />
                         </div>
-                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-3xl absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all'>Cookie & Cream</p>
+                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-sm xl:text-3xl absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all'>Cookie & Cream</p>
                     </div>
-                    <div className='w-[350px] h-[350px] overflow-hidden relative product rounded-xl'>
-                        <div className='w-[350px] h-[350px] overflow-hidden product-img '>
+                    <div className='w-[130px] h-[150px] xl:w-[350px] xl:h-[350px] overflow-hidden relative product rounded-xl'>
+                        <div className='w-full h-full overflow-hidden product-img '>
                             <img className='h-full w-full object-cover object-center transition-all duration-500' src="/images/lemon-meringue-marshmallow.jpg" alt="" />
                         </div>
-                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-3xl z-20 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all tracking-wide'>Lemon Meringue</p>
+                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-sm xl:text-3xl z-20 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all tracking-tighter'>Lemon Meringue</p>
                     </div>
-                    <div className='w-[350px] h-[350px] overflow-hidden relative product rounded-xl'>
-                        <div className='w-[350px] h-[350px] overflow-hidden product-img '>
+                    <div className='w-[130px] h-[150px] xl:w-[350px] xl:h-[350px] overflow-hidden relative product rounded-xl'>
+                        <div className='w-full h-full overflow-hidden product-img '>
                             <img className='h-full w-full object-cover object-center transition-all duration-500' src="/images/strawberry-cheesecake-marshmallow.jpg" alt="" />
                         </div>
-                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-3xl z-20 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all tracking-wide'>Strawberry Cheescake</p>
+                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-[13px] xl:text-3xl z-20 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all tracking-tighter'>Strawberry Cheescake</p>
                     </div>
-                    <div className='w-[350px] h-[350px] overflow-hidden relative product rounded-xl'>
-                        <div className='w-[350px] h-[350px] overflow-hidden product-img '>
+                    <div className='w-[130px] h-[150px] xl:w-[350px] xl:h-[350px] overflow-hidden relative product rounded-xl'>
+                        <div className='w-full h-full overflow-hidden product-img '>
                             <img className='h-full w-full object-cover object-center transition-all duration-500' src="/images/nutella-chocochip.jpg" alt="" />
                         </div>
-                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-3xl z-20 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all tracking-wide'>Nutella Chocochip</p>
+                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-sm xl:text-3xl z-20 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all tracking-tighter'>Nutella Chocochip</p>
                     </div>
                 </div>
             </div>
             <div className='px-2 py-3 xl:px-10 xl:py-4 bg-red-800/5 mx-5 rounded-xl mt-8'>
-                <h1 className='text-4xl xl:text-5xl mb-8 font-semibold font-["Outfit"]'>New York Style Cookies </h1>
+                <h1 className='text-4xl xl:text-5xl mb-10 font-medium font-["Outfit"] opacity-75'>New York Style Cookies </h1>
                 <div className='flex flex-wrap gap-8 justify-center'>
-                    <div className='w-[350px] h-[350px] overflow-hidden relative product rounded-xl'>
-                        <div className='w-[350px] h-[350px] overflow-hidden product-img '>
+                    <div className='w-[130px] h-[150px] xl:w-[350px] xl:h-[350px] overflow-hidden relative product rounded-xl'>
+                        <div className='w-full h-full overflow-hidden product-img '>
                             <img className='h-full w-full object-cover object-center transition-all duration-500' src="/images/chocochip-walnut-cookie.jpg" alt="" />
                         </div>
-                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-3xl z-20 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all tracking-wide'>Chocochip Walnut</p>
+                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-sm xl:text-3xl z-20 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all tracking-tighter'>Chocochip Walnut</p>
                     </div>
-                    <div className='w-[350px] h-[350px] overflow-hidden relative product rounded-xl'>
-                        <div className='w-[350px] h-[350px] overflow-hidden product-img '>
+                    <div className='w-[130px] h-[150px] xl:w-[350px] xl:h-[350px] overflow-hidden relative product rounded-xl'>
+                        <div className='w-full h-full overflow-hidden product-img '>
                             <img className='h-full w-full object-cover object-center transition-all duration-500' src="/images/coconut-and-llime-cookie.jpg" alt="" />
                         </div>
-                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-3xl z-20 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all tracking-wide'>Coconut & Lime</p>
+                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-sm xl:text-3xl z-20 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all tracking-tighter'>Coconut & Lime</p>
                     </div>
-                    <div className='w-[350px] h-[350px] overflow-hidden relative product rounded-xl'>
-                        <div className='w-[350px] h-[350px] overflow-hidden product-img '>
+                    <div className='w-[130px] h-[150px] xl:w-[350px] xl:h-[350px] overflow-hidden relative product rounded-xl'>
+                        <div className='w-full h-full overflow-hidden product-img '>
                             <img className='h-full w-full object-cover object-center transition-all duration-500' src="/images/dark-chocolate-cookie.jpg" alt="" />
                         </div>
-                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-3xl absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all'>Dark Chocolate</p>
+                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-sm xl:text-3xl absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all'>Dark Chocolate</p>
                     </div>
-                    <div className='w-[350px] h-[350px] overflow-hidden relative product rounded-xl'>
-                        <div className='w-[350px] h-[350px] overflow-hidden product-img '>
+                    <div className='w-[130px] h-[150px] xl:w-[350px] xl:h-[350px] overflow-hidden relative product rounded-xl'>
+                        <div className='w-full h-full overflow-hidden product-img '>
                             <img className='h-full w-full object-cover object-center transition-all duration-500' src="/images/marshmallow-stuffed-cookie.jpg" alt="" />
                         </div>
-                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-3xl z-20 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all tracking-wide'>Marshmallow Stuffed</p>
+                        <p className='text-[var(--font-color)] bg-pink-50/70 px-8 py-3 rounded-full text-sm xl:text-3xl z-20 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] transition-all tracking-tighter'>Marshmallow Stuffed</p>
                     </div>
                 </div>
             </div>
