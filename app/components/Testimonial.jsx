e91e8c777d9cd9e4093b0d6e9b332a923de40759
Vinayak Nawdhar <vinayaknawdhar003@gmail.com>
2024-06-03 14:31:07 +0530

    social media icons added

--- app/components/Testimonial.jsx
+++ app/components/Testimonial.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 
 const Testimonial = () => {
     return (
-        <section id='reviews' class="bg-white/90 py-16 md:scroll-m-[110px] scroll-m-[80px]">
+        <section id='reviews' className="bg-white/90 py-16 md:scroll-m-[110px] scroll-m-[80px]">
             <h1 className='text-3xl xl:text-5xl font-extrabold text-center font-["Outfit"] tracking-tighter drop-shadow-2xl'>WHAT OUR CUSTOMER SAYS</h1>
             <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6 flex gap-8 flex-wrap">
                 {testimonials.map(test => (
-                    <figure key={test.id} class="hover-target bg-[#FEFAF9] p-4 rounded-2xl shadow-2xl xl:h-96 xl:w-96 relative pb-20 xl:pb-0">
+                    <figure key={test.id} className="hover-target bg-[#FEFAF9] p-4 rounded-2xl shadow-2xl xl:h-96 xl:w-96 relative pb-20 xl:pb-0">
                         <svg className="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor" />
                         </svg>
